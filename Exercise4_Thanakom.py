4364461d6f4ca0b6b25192bf98172e89b567cfb4
BaBenz <35694813+BaBenz@users.noreply.github.com>
2022-10-16 17:26:41 +0700

    Create Exercise4_Thanakom.py

---
+++ Exercise4_Thanakom.py
@@ -0,0 +1,10 @@
+a = 86.13
+b = 82.65
+c = 85.94
+d = 84.23
+
+#output
+print("Foundation English",                 "                 Score :",a)
+print("General Business",                   "                   Score :",b)
+print("Introduction to Computer Systems",   "   Score :",c)
+print("Computer Programming",               "               Score :",d)
